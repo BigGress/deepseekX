9d9f846afbec12886fa5a1b46c4860b0a5112020
BigGress <t451148156@gmail.com>
2026-05-31 14:03:51 +0800

    fix: add props interface to ResizeHandle

--- src/components/ResizeHandle.tsx
+++ src/components/ResizeHandle.tsx
@@ -1,4 +1,6 @@
-export default function ResizeHandle() {
+interface ResizeHandleProps {}
+
+export default function ResizeHandle(_props: ResizeHandleProps) {
   return (
     <div className="w-1 h-full cursor-col-resize flex items-center justify-center">
       <div className="w-px h-full bg-neutral-700 hover:bg-blue-500 active:bg-blue-400 transition-colors duration-150" />
